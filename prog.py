b029fb5b709ef9fa713660ddc25bd355a84a06d3
phamtq <phamtq.mobile@gmail.com>
2025-11-21 11:34:17 -0800

    Inital files

---
+++ prog.py
@@ -0,0 +1,19 @@
+# PCC Fall 2025 Hackathon Project
+# Date: November 21, 2025
+#
+# Group members: Kieran, 
+#
+# Description: This project uses the DOFBOT-Pi to stack colored blocks in a
+# particular order using computer vision.
+
+#!/usr/bin/env python3
+#coding=utf-8
+
+import time
+from Arm_Lib import Arm_Device
+
+# Create the Arm object
+Arm = Arm_Device()
+time.sleep(.1)
+
+
